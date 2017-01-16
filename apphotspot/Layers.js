d4bf57812a0dca536c944b31824e82cb294b7f10
chingchai <chingchai.h@gmail.com>
2017-01-17 00:12:41 +0700

    final wms service for hotspot data

--- apphotspot/Layers.js
+++ apphotspot/Layers.js
@@ -1,11 +1,19 @@
+/* Hotspot fire app
+ * Mr Chingchai Humhong
+ * Assoc Prof Dr Chada Narongrit
+ * 16/01/2017
+ * chada@nu.ac.th, chingchai.h@gmail.com, chingchaih@nu.ac.th
+ * GISTNU @ Naresuan University
+ * MIT@License
+ * github : https://github.com/chingchai/ActiveFire
+ */
 Ext.namespace("Heron.options");
 Ext.namespace("Heron.scratch");
 Ext.namespace("Heron.examples");
-
 OpenLayers.Util.onImageLoadErrorColor = "transparent";
 OpenLayers.ProxyHost = "resources/proxy.php?url=";
+//OpenLayers.ProxyHost = "/cgi-bin/proxy.cgi?url=";
 OpenLayers.DOTS_PER_INCH = 25.4 / 0.28;
-
 Ext.BLANK_IMAGE_URL = 'http://cdnjs.cloudflare.com/ajax/libs/extjs/3.4.1-1/resources/images/default/s.gif';
 
 Ext.namespace("Heron.options.map");
@@ -15,7 +23,6 @@ Heron.options.map.settings = {
   displayProjection: new OpenLayers.Projection("EPSG:4326"),
 	units: 'm',
 	maxExtent: '-20037508.34, -20037508.34, 20037508.34, 20037508.34',
-  //displayProjection1: new OpenLayers.Projection("EPSG:32647"),
   center: '11141190.727,1458223.243',
   maxResolution: '156543.0339',//'0.17578125',
   xy_precision: 5,
@@ -49,10 +56,7 @@ Heron.options.wfs.downloadFormats = [
 Heron.scratch.urls = {
 	OWS: 'http://www2.cgistln.nu.ac.th/geoserver/ows?',
   OWS2: 'http://ows.gistda.or.th/geoserver/ows?',
-  OWS3: 'http://www.map.nu.ac.th/geoserver-hgis/ows?',
-  GS2_OWS: 'http://kademo.nl/gs2/ows?',
-
-  NASAOWS: 'https://firms.modaps.eosdis.nasa.gov/wmsc6/?',
+  OWS3: 'http://www.map.nu.ac.th/geoserver-fire/ows?',
 };
 
 Heron.scratch.layermap = {
@@ -63,7 +67,7 @@ Heron.scratch.layermap = {
    */
 
    // OSM Base Layers
-   //osm:  new OpenLayers.Layer.OSM("OpenStreetMap",null),
+   osm:  new OpenLayers.Layer.OSM("OpenStreetMap",null),
 
 	// Google Base Layers
 	gstr: new OpenLayers.Layer.Google(
@@ -86,24 +90,6 @@ Heron.scratch.layermap = {
 			{type: google.maps.MapTypeId.TERRAIN, visibility: false},
 			{singleTile: false, buffer: 0, isBaseLayer: true}
 	),
-/*
-	// Bing Layers
-	 bgr: new OpenLayers.Layer.Bing({
-        name: "Bing Road",
-        key: "Ai8NCnEOhBj22Uz9OIfrhCBxvjyDQJEDkBbTWzJCfuKdlyR_VyqFxMvQgbmgJ10z",
-        type: "Road"
-    }),
-    bgh: new OpenLayers.Layer.Bing({
-        name: "Bing Hybrid",
-        key: "Ai8NCnEOhBj22Uz9OIfrhCBxvjyDQJEDkBbTWzJCfuKdlyR_VyqFxMvQgbmgJ10z",
-        type: "AerialWithLabels"
-    }),
-    bgs: new OpenLayers.Layer.Bing({
-        name: "Bing Aerial",
-        key: "Ai8NCnEOhBj22Uz9OIfrhCBxvjyDQJEDkBbTWzJCfuKdlyR_VyqFxMvQgbmgJ10z",
-        type: "Aerial"
-    }),
-*/
   none: new OpenLayers.Layer.Image(
         "None",
         Ext.BLANK_IMAGE_URL,
@@ -135,9 +121,9 @@ Heron.scratch.layermap = {
   amp: new OpenLayers.Layer.WMS(
               "amphoe",
               Heron.scratch.urls.OWS2,
-              {layers: "	fgds54_beta:L05_AdminBoundary_GISTDA_Amphoe_2011_50k", transparent: true, format: 'image/png'},
+              {layers: "fgds54_beta:L05_AdminBoundary_GISTDA_Amphoe_2011_50k", transparent: true, format: 'image/png'},
               {singleTile: false, opacity: 0.9, isBaseLayer: false, visibility: false, noLegend: false,
-  			featureInfoFormat: 'application/vnd.ogc.gml', transitionEffect: 'null', metadata: {
+  			           featureInfoFormat: 'application/vnd.ogc.gml', transitionEffect: 'null', metadata: {
                   wfs: {
                       protocol: 'fromWMSLayer',
                       downloadFormats: Heron.options.wfs.downloadFormats
@@ -149,79 +135,37 @@ Heron.scratch.layermap = {
                 Heron.scratch.urls.OWS2,
                 {layers: "fgds54_beta:L05_AdminBoundary_GISTDA_Tambon_2011_50k", transparent: true, format: 'image/png'},
                 {singleTile: false, opacity: 0.9, isBaseLayer: false, visibility: false, noLegend: false,
-    			featureInfoFormat: 'application/vnd.ogc.gml', transitionEffect: 'null', metadata: {
+    			           featureInfoFormat: 'application/vnd.ogc.gml', transitionEffect: 'null', metadata: {
                     wfs: {
                         protocol: 'fromWMSLayer',
                         downloadFormats: Heron.options.wfs.downloadFormats
                     }
                 }}
         ),
-// Climate layers
-
-MODIS_Hotspots: new OpenLayers.Layer.WMS(
-        "fires24",
-        Heron.scratch.urls.NASAOWS,
-        {layers: "fires48", format: "image/png", transparent: true},
-        {isBaseLayer: false, singleTile: true, visibility: false, alpha: true, opacity: 0.7,
-          featureInfoFormat: "application/vnd.ogc.gml", transitionEffect: 'resize',
-            metadata: {
-                wfs: {
-                    protocol: 'fromWMSLayer',
-                    featurePrefix: 'heron',
-                    featureNS: 'http://heron-mc.org',
-                    downloadFormats: Heron.options.wfs.downloadFormats
-                }
-            }}
-  ),
-
-
-
-/*
- * Aardbevingen
- *  KNMI Aardbevingen: vanuit http://www.knmi.nl/seismologie/aardbevingen-nederland.html
- */
-knmi: new OpenLayers.Layer.WMS(
-        "KNMI Aardbevingen",
-        Heron.scratch.urls.GS2_OWS,
-        {layers: "heron:knmi_aardbevingen_4326", format: "image/png", transparent: true},
-        {isBaseLayer: false, singleTile: true, visibility: false, alpha: true, opacity: 0.7,
-          featureInfoFormat: "application/vnd.ogc.gml", transitionEffect: 'resize',
-            metadata: {
-                wfs: {
-                    protocol: 'fromWMSLayer',
-                    featurePrefix: 'heron',
-                    featureNS: 'http://heron-mc.org',
-                    downloadFormats: Heron.options.wfs.downloadFormats
-                }
-            }}
-  ),
-
-timslider_pnt: new OpenLayers.Layer.WMS(
-          "v_all_dengue_timslider_pnt",
+hotspot: new OpenLayers.Layer.WMS(
+          "fire_archive",
           Heron.scratch.urls.OWS3,
-          {layers: "hgis:v_all_dengue_timslider_pnt", transparent: true, format: 'image/png'},
-          {singleTile: false, opacity: 0.9, isBaseLayer: false, visibility: true, noLegend: false,
-    featureInfoFormat: 'application/vnd.ogc.gml', transitionEffect: 'null', metadata: {
-              wfs: {
-                  protocol: 'fromWMSLayer',
-                  downloadFormats: Heron.options.wfs.downloadFormats
-              }
-          }}
-  ),
-timslider: new OpenLayers.Layer.WMS(
-          "v_all_dengue_timslider",
-          Heron.scratch.urls.OWS3,
-          {layers: "hgis:v_all_dengue_timslider", transparent: true, format: 'image/png'},
+          {layers: "fire:fire_archive", transparent: true, format: 'image/png'},
           {singleTile: false, opacity: 0.9, isBaseLayer: false, visibility: false, noLegend: false,
-    featureInfoFormat: 'application/vnd.ogc.gml', transitionEffect: 'null', metadata: {
+            featureInfoFormat: 'application/vnd.ogc.gml', transitionEffect: 'null', metadata: {
               wfs: {
                   protocol: 'fromWMSLayer',
                   downloadFormats: Heron.options.wfs.downloadFormats
               }
           }}
   ),
-
-
+heatmap: new OpenLayers.Layer.WMS(
+          "fire_heatmap",
+          Heron.scratch.urls.OWS3,
+          {layers: "fire:fire_heatmap", transparent: true, format: 'image/png'},
+          {singleTile: true, opacity: 0.9, isBaseLayer: false, visibility: false, noLegend: false,
+            featureInfoFormat: 'application/vnd.ogc.gml', transitionEffect: 'null', metadata: {
+              wfs: {
+                  protocol: 'fromWMSLayer',
+                  downloadFormats: Heron.options.wfs.downloadFormats
+              }
+          }}
+  ),
 
 /*    dist: new OpenLayers.Layer.WMS(
           "_nddi",
@@ -246,26 +190,19 @@ timslider: new OpenLayers.Layer.WMS(
  * ====================================================================
  */
 Heron.options.map.layers = [
-  //add overlay1
-  Heron.scratch.layermap.knmi,
-  Heron.scratch.layermap.MODIS_Hotspots,
-
-  //add overlay2
+  //add overlays
 	Heron.scratch.layermap.prov,
   Heron.scratch.layermap.amp,
   Heron.scratch.layermap.tam,
-  Heron.scratch.layermap.timslider_pnt,
-  Heron.scratch.layermap.timslider,
+  Heron.scratch.layermap.heatmap,
+  Heron.scratch.layermap.hotspot,
 	//BaseLayers
-//  Heron.scratch.layermap.osm
+  Heron.scratch.layermap.gter,
+  Heron.scratch.layermap.osm,
 	Heron.scratch.layermap.gstr,
 	Heron.scratch.layermap.ghyb,
 	Heron.scratch.layermap.gsat,
-	Heron.scratch.layermap.gter,
   Heron.scratch.layermap.none
-  /*	Heron.scratch.layermap.bgr,
-	Heron.scratch.layermap.bgh,
-	Heron.scratch.layermap.bgs */
 
 ];
 
@@ -282,18 +219,16 @@ var treeTheme = [
 					children:
 						[
 							{nodeType: "gx_layer", layer: "provice", text: "ขอบเขตจังหวัด", legend: true},
-              {nodeType: "gx_layer", layer: "amphoe", text: "ขอบเขตอำเเภอ", legend: true},
+              {nodeType: "gx_layer", layer: "amphoe", text: "ขอบเขตอำเภอ", legend: true},
               {nodeType: "gx_layer", layer: "tambon", text: "ขอบเขตตำบล", legend: true},
 						]
 				},{
-					text:'ชั้นข้อมูลภูมิอากาศ',
+					text:'ชั้นข้อมูลจุดความร้อน MODIS',
 					expanded: true,
 					children:
 						[
-              {nodeType: "gx_layer", layer: "fires24", text: "MODIS_Hotspots", legend: true},
-              {nodeType: "gx_layer", layer: "v_all_dengue_timslider_pnt", text: "v_all_dengue", legend: true},
-              {nodeType: "gx_layer", layer: "v_all_dengue_timslider", text: "v_all_dengue_timslider", legend: true},
-              {nodeType: "gx_layer", layer: "KNMI Aardbevingen", text: "KNMI Aardbevingen", legend: true},
+              {nodeType: "gx_layer", layer: "fire_archive", text: "จุดความร้อน", legend: true},
+              {nodeType: "gx_layer", layer: "fire_heatmap", text: "Heatmap", legend: true},
 
 						]
 				}
